9b84d85b69fb4c59c1af23e7f739430d9fb89e4b
lulala <czps40511@gmail.com>
2025-03-20 01:04:47 +0800

    demoTwo

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,7 +15,7 @@ export default defineConfig({
       },
     },
   },
-
-  plugins: [vue(), eslint()],
+  base: "./",
+  plugins: [vue()],
 });
 // eslint()
