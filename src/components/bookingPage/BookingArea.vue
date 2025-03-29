ebcccc72e945f66ca1ea2cc536a5e1ef0b90a925
lulala <czps40511@gmail.com>
2025-03-29 10:17:13 +0800

    changSelect

--- src/components/bookingPage/BookingArea.vue
+++ src/components/bookingPage/BookingArea.vue
@@ -41,20 +41,8 @@ const cityName = ref();
 const cityWeather = ref();
 const cityIdx = ref(0);
 const checkLimit = (e: Event) => {
-  const peopleNum = Number((e.target as HTMLSelectElement).value);
+  const peopleNum = Number((e.target as HTMLSelectElement).value);  
   const type = (e.target as HTMLSelectElement).name;
-  if (type === "adults") {
-    childs.value = Math.abs(peopleNum - 6);
-    EatAdults.value = peopleNum;
-  }
-  if (type === "childs") {
-    adults.value = Math.abs(peopleNum - 6);
-    Eatchilds.value = peopleNum;
-  }
-};
-const checkLimitMobile = (e: Event) => {
-  const peopleNum = Number((e.target as HTMLOptionElement).value);
-  const type = (e.target as HTMLOptionElement).id;
   if (type === "adults") {
     childs.value = Math.abs(peopleNum - 6);
     EatAdults.value = peopleNum;
@@ -258,12 +246,12 @@ onUnmounted(() => {
         <div class="lg:w-[49%] xl:mt-10">
           <span class="block mb-2 lg:text-3xl">用餐人數</span>
           <div class="w-full flex gap-2 lg:mt-7">
-            <select class="w-1/2 pl-1 rounded-md bg-white lg:w-1/4 lg:py-2 lg:text-xl lg:grow" name="adults" @click.stop="checkLimit">
-              <option v-for="n in adults" class="bg-white" :key="n" :value="n" :selected="n == 2" @click="checkLimitMobile">{{ n }}位大人</option>
+            <select ref="selectAdultDom" class="w-1/2 pl-1 rounded-md bg-white lg:w-1/4 lg:py-2 lg:text-xl lg:grow" name="adults" @change.stop="checkLimit">
+              <option v-for="n in adults" class="bg-white" :key="n" :value="n" :selected="n == 2">{{ n }}位大人</option>
             </select>
-            <select class="w-1/2 pl-1 rounded-md bg-white lg:w-1/4 lg:py-2 lg:text-xl lg:grow" name="childs" @click.stop="checkLimit">
-              <option class="bg-white" :value="0" @click="checkLimitMobile">0位小孩</option>
-              <option v-for="n in childs" class="bg-white" :key="n" :value="n" @click="checkLimitMobile">{{ n }}位小孩</option>
+            <select ref="selectChildtDom" class="w-1/2 pl-1 rounded-md bg-white lg:w-1/4 lg:py-2 lg:text-xl lg:grow" name="childs" @change.stop="checkLimit">
+              <option class="bg-white" :value="0" >0位小孩</option>
+              <option v-for="n in childs" class="bg-white" :key="n" :value="n">{{ n }}位小孩</option>
             </select>
           </div>
         </div>
